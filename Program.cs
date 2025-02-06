72021f1d158ca7e27115d6e772beaf1f14719d0d
Richie_Vist <vistovschiiserghei@gmail.com>
2025-02-06 19:20:26 +0200

    Expand to upper and lower case characters

--- Program.cs
+++ Program.cs
@@ -1,67 +1,75 @@
-﻿namespace Vigenere_cipher;
+﻿using System;
+using System.Linq;
 
-public class VigenereСipher
+namespace Vigenere_cipher
 {
-    private const int SIZE_ABCrus = 33;
-    private const int SIZE_ABCeng = 27;
+public class VigenereCipher
+{
+    private static readonly char[] ABCrus = new char[]
+    {
+        'А','Б','В','Г','Д','Е','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я',
+        'а','б','в','г','д','е','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я',
+        ' '
+    };
+    
+    private static readonly char[] ABCeng = new char[]
+    {
+        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+        ' '
+    };
     private string _key;
-    private  char[] _alphabet;
+    private char[] _alphabet;
 
-    private static readonly char[] ABCrus = new char[SIZE_ABCrus]
-    {
-        'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н',
-        'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы',
-        'ь', 'э', 'ю', 'я', ' '
-    };
-
-    private static readonly char[] ABCeng = new char[SIZE_ABCeng]
-    {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '
-    };
-
+    
     private char[] DetermineAlphabet(string text)
     {
-        text = text.ToLower(); 
-        return text.Any(c => ABCrus.Contains(c)) ? ABCrus : ABCeng;
+        bool containsRussian = text.Any(c => (c >= 'А' && c <= 'Я') || (c >= 'а' && c <= 'я'));
+        return containsRussian ? ABCrus : ABCeng;
     }
 
-    public VigenereСipher(string key)
+    public VigenereCipher(string key)
     {
-        _key = key.ToLower(); 
-        _alphabet = DetermineAlphabet(key); 
+        _key = key; 
+        _alphabet = DetermineAlphabet(key);
     }
 
     public string Encrypt(string text)
     {
-        _alphabet = DetermineAlphabet(text); 
-        return Process(text.ToLower(), _key, true);
+        
+        _alphabet = DetermineAlphabet(text);
+        return Process(text, _key, true);
     }
 
     public string Decrypt(string text)
     {
-        _alphabet = DetermineAlphabet(text); 
-        return Process(text.ToLower(), _key, false);
+        _alphabet = DetermineAlphabet(text);
+        return Process(text, _key, false);
     }
 
     private string Process(string text, string key, bool encrypt)
     {
         string result = "";
         int alphabetLength = _alphabet.Length;
-
+        
         for (int i = 0, j = 0; i < text.Length; i++)
         {
             char textChar = text[i];
             int textIndex = Array.IndexOf(_alphabet, textChar);
 
+            
             if (textIndex == -1)
             {
-                result += textChar; 
+                result += textChar;
                 continue;
             }
-
+            
             char keyChar = key[j % key.Length];
             int keyIndex = Array.IndexOf(_alphabet, keyChar);
+
+            
+            if (keyIndex == -1)
+                keyIndex = 0;
 
             int newIndex = encrypt
                 ? (textIndex + keyIndex) % alphabetLength
@@ -73,19 +81,30 @@ public class VigenereСipher
 
         return result;
     }
+
+    public void SetKey(string key)
+    {
+        _key = key;
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        var vigenere = new VigenereСipher("key");
-        var text = "text from vigenere cipher";
+        var vigenereEng = new VigenereCipher("Key");
+        var textEng = "fFDFDFWEFWEFWER";
+        var encryptedTextEng = vigenereEng.Encrypt(textEng);
+        Console.WriteLine($"Зашифрованный текст: {encryptedTextEng}");
+        var decryptedTextEng = vigenereEng.Decrypt(encryptedTextEng);
+        Console.WriteLine($"Расшифрованный текст: {decryptedTextEng}");
 
-        var encryptedText = vigenere.Encrypt(text);
-        Console.WriteLine($"Зашифрованный текст: {encryptedText}");
-
-        var decryptedText = vigenere.Decrypt(encryptedText);
-        Console.WriteLine($"Расшифрованный текст: {decryptedText}");
+        var vigenereRus = new VigenereCipher("Ключ");
+        var textRus = "Прощай Друг";
+        var encryptedTextRus = vigenereRus.Encrypt(textRus);
+        Console.WriteLine($"Зашифрованный текст: {encryptedTextRus}");
+        var decryptedTextRus = vigenereRus.Decrypt(encryptedTextRus);
+        Console.WriteLine($"Расшифрованный текст: {decryptedTextRus}");
     }
+}
 }
