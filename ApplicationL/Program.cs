2198d62bf6268963029ef5236c3970ed790e55b0
Richie_Vist <vistovschiiserghei@gmail.com>
2025-02-07 00:01:03 +0200

    Code splitting, pattern insertion. Console interface

---
+++ ApplicationL/Program.cs
@@ -0,0 +1,82 @@
+﻿using CipherLib;
+using CipherLib.Factory;
+using CipherLib.Service;
+
+namespace ApplicationL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            string? choice = null;
+            ICipher cipher = null;
+            string key = "";
+            do
+            {
+                Console.WriteLine("Select encryption algorithm:");
+                Console.WriteLine("1. Vigenère cipher");
+                choice = Console.ReadLine();
+                if (choice == "1")
+                {
+                    Console.Write("Enter the key: ");
+                    key = Console.ReadLine();
+                    cipher = CipherFactory.CreateCipher("vigenere", key);
+                }
+                else if (choice == "0")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("!!!!Wrong choice!!!!");
+                    choice = null;
+                    Console.WriteLine("If you want to exit, enter 0.");
+                }
+            }while (choice == null);
+
+            var service = new CipherService(cipher);
+            
+            while (true)
+            {
+                Console.WriteLine("\nSelect an action:");
+                Console.WriteLine("1. Encrypt text");
+                Console.WriteLine("2. Decipher the text");
+                Console.WriteLine("3. Change key");
+                Console.WriteLine("0. Exit");
+
+                var input = Console.ReadLine();
+                string? text = null;
+                if (input == "0")
+                    break;
+               
+                
+                if (input == "1")
+                {
+                    Console.Write("Enter the text: ");
+                    text = Console.ReadLine();
+                    var encrypted = service.EncryptText(text);
+                    Console.WriteLine($"Encrypted text: {encrypted}");
+                }
+                else if (input == "2")
+                {
+                    Console.Write("Enter encrypted text: ");
+                    text = Console.ReadLine();
+                    var decrypted = service.DecryptText(text);
+                    Console.WriteLine($"Decrypted text: {decrypted}");
+                }
+                else if (input == "3")
+                {
+                    Console.Write("Enter new key: ");
+                    key = Console.ReadLine();
+                    cipher.SetKey(key);
+                }
+                else
+                {
+                    Console.WriteLine("!!!!!!!Invalid command!!!!!!!!!");
+                }
+            }
+        }
+    }
+}
---
+++ CipherLib/Service/CipherService.cs
@@ -0,0 +1,27 @@
+﻿namespace CipherLib.Service
+{
+    public class CipherService
+    {
+        private readonly ICipher _cipher;
+        
+        public CipherService(ICipher cipher)
+        {
+            _cipher = cipher;
+        }
+
+        public string EncryptText(string text)
+        {
+            return _cipher.Encrypt(text);
+        }
+
+        public string DecryptText(string text)
+        {
+            return _cipher.Decrypt(text);
+        }
+
+        public void SetKey(string key)
+        {
+            _cipher.SetKey(key);
+        }
+    }
+}
